6512c31787837e9c16ece052d3823d23e3d66928
bala charan <insanecharan@gmail.com>
2026-04-14 12:11:16 +0530

    fix home route

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "secret123"
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 # ---------------- DATABASE ----------------
 def init_db():
